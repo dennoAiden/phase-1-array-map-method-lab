49418d77429c0c8a1c0cf0d2a7cca9832f13942b
(dennoAiden) <(kipkiruidennis25@gmail.com)>
2024-07-02 20:58:56 +0300

    adds index

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => 
+    tutorial
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ')
+  );
+};
+ const titleCasedTutorials = tutorials.map(titleCased);
+ console.log(titleCasedTutorials);
